56cf943c48e8c5dc8f3109cedcf2febccc849c8d
Nickeymaths <ifromchauphong@gmail.com>
2025-05-04 20:47:29 +0700

    fix: hanging

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"context"
+	"log"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -13,7 +15,9 @@ func TestTransferTx(t *testing.T) {
 	account1 := CreateRandomAccount(t)
 	account2 := CreateRandomAccount(t)
 
-	n := 5
+	log.Printf("before: %v, %v", account1.Balance, account2.Balance)
+
+	n := 2
 	amount := int64(10)
 
 	errs := make(chan error)
@@ -21,23 +25,25 @@ func TestTransferTx(t *testing.T) {
 
 	for i := 0; i < n; i++ {
 		go func() {
-			arg := TransferTxParams{
+			result, err := testStore.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
-			}
-			result, err := testStore.TransferTx(context.Background(), arg)
+			})
 
 			results <- result
 			errs <- err
 		}()
 	}
+
 	for i := 0; i < n; i++ {
 		err := <-errs
-		result := <-results
-
 		require.NoError(t, err)
+
+		result := <-results
 		require.NotEmpty(t, result)
+
+		log.Printf("tx %d: %v, %v", i, result.FromAccount.Balance, result.ToAccount.Balance)
 
 		transfer := result.Transfer
 		require.NotEmpty(t, transfer)
@@ -47,6 +53,9 @@ func TestTransferTx(t *testing.T) {
 		require.NotZero(t, transfer.ID)
 		require.NotZero(t, transfer.CreatedAt)
 
+		_, err = testStore.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
 		fromEntry := result.FromEntry
 		require.NotEmpty(t, fromEntry)
 		require.Equal(t, account1.ID, fromEntry.AccountID)
@@ -54,11 +63,34 @@ func TestTransferTx(t *testing.T) {
 		require.NotZero(t, fromEntry.ID)
 		require.NotZero(t, fromEntry.CreatedAt)
 
+		_, err = testStore.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
 		toEntry := result.ToEntry
 		require.NotEmpty(t, toEntry)
-		require.Equal(t, account1.ID, toEntry.AccountID)
-		require.Equal(t, -amount, toEntry.Amount)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
 		require.NotZero(t, toEntry.ID)
 		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = testStore.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// Check account balance
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+		require.Equal(t, account1.Owner, fromAccount.Owner)
+		require.Equal(t, amount, account1.Balance-fromAccount.Balance)
+		require.Equal(t, account1.Currency, fromAccount.Currency)
+		require.WithinDuration(t, account1.CreatedAt, fromAccount.CreatedAt, time.Second)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+		require.Equal(t, account2.Owner, toAccount.Owner)
+		require.Equal(t, amount, toAccount.Balance-account2.Balance)
+		require.Equal(t, account2.Currency, toAccount.Currency)
+		require.WithinDuration(t, account2.CreatedAt, toAccount.CreatedAt, time.Second)
 	}
 }
